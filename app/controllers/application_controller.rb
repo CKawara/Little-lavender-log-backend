8b17ae669fd896b3212160c0437f9898cd632636
Catherine Kawara <ndutakawara2@gmail.com>
2022-06-26 05:34:02 +0300

    add database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < Sinatra::Base
     set :default_content_type, 'application/json'
-    
-    get '/categories' do
-      categories = Category.all
-      categories.to_json
+
+    get '/' do
+        categories = Category.all
+        categories.to_json
     end
-    
-  end
+
+end
   
